6a176df9f5ae925f4ec007e2ccac1e3b43073774
JosepRIvaille <josep.de.cid@gmail.com>
2016-10-09 02:15:19 +0200

    Delete unnecessary routes

--- webapp/resources/js/controllers/FormController.js
+++ webapp/resources/js/controllers/FormController.js
@@ -1,26 +1,55 @@
 app.controller('FormController', function($scope, $http) {
 
-    $scope.cities = [{id: 'city1'}, {id: 'city2'}];
+    $scope.cities = [{id: ''}];
 
     $scope.addNewChoice = function () {
-        console.log("dd");
-        var newItemNo = $scope.cities.length + 1;
-
-        $scope.cities.push({'id': 'choice' + newItemNo});
+        var currentItemNo = $scope.cities.length - 1;
+        //if ($scope.cities[currentItemNo]['id'] != "") { //
+        //    $scope.cities[currentItemNo]['id'] = 'city_' - currentItemNo;
+        //    $scope.cities.push({'id': ''});
+        //}
+        $scope.cities.push({'id': ''});
     };
 
     $scope.removeChoice = function() {
         var lastItem = $scope.cities.length - 1;
-        $scope.cities.splice(lastItem);
+        if (lastItem >= 1) {
+            $scope.cities.splice(lastItem)
+        }
     };
 
     $scope.uploadData = function() {
+        var loader = document.querySelector("#loader");
+        console.log(loader);
+        loader.className += ' display_loader';
         $http({ method: 'POST', url: 'http://127.0.0.1:5000/sendData' })
             .success(function(data, status, headers, config) {
                 console.log(data + " " + status);
             }).error(function(data, status, headers, config) {
             console.log("Faaaail " + status);
         });
+        $scope.cities = [{id: ''}];
     };
+
+    $scope.receiveData = function () {
+        listener();
+
+    };
+
+    var listener = function() {
+        var server = app.listen(3000, function () {
+            var host = server.address().address;
+            var port = server.address().port;
+            console.log('Example app listening at http://%s:%s', host, port);
+        });
+    };
+
+    $scope.onEnterClick = function (event) {
+        if (event.keyCode == 13) $scope.addNewChoice();
+        else if (event.keyCode == 8) {
+            $scope.removeChoice();
+            event.preventDefault();
+        }
+    }
 
 });
